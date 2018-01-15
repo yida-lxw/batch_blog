38a77612fc3929bcf2a7fbebeece96f29f8331bb
yida <949226420@qq.com>
2018-01-16 00:03:06 +0800

    add system constant class.

---
+++ src/main/java/com/yida/framework/blog/utils/Constant.java
@@ -0,0 +1,21 @@
+package com.yida.framework.blog.utils;
+
+/**
+ * @Author Lanxiaowei
+ * @Date 2018-01-16 00:01
+ * @Description 系统常量类
+ */
+public class Constant {
+    /**
+     * 拉丁字符编码
+     */
+    public static final String DEFAULT_CHARSET_ISO = "ISO-8859-1";
+    /**
+     * UTF-8编码
+     */
+    public static final String DEFAULT_CHARSET_UTF8 = "UTF-8";
+    /**
+     * 默认读写缓冲区大小
+     */
+    public static final int BUFFER_SIZE = 4096;
+}
