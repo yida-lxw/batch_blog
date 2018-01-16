11fae1360acf9625ca6d17fc354092493d1a7725
yida <949226420@qq.com>
2018-01-16 22:35:41 +0800

    add Blog config JavaBean

--- src/main/java/com/yida/framework/blog/config/BlogConfig.java
+++ src/main/java/com/yida/framework/blog/config/BlogConfig.java
@@ -6,5 +6,64 @@ package com.yida.framework.blog.config;
  * @Description Blog配置实体类
  */
 public class BlogConfig {
+    /**
+     * Pandoc的安装目录
+     */
+    private String pandocHome;
+    /**
+     * Word文档的存储根目录
+     */
+    private String wordBasePath;
+    /**
+     * 博客发送日期
+     */
+    private String blogSendDate;
+    /**
+     * Github的登录账号
+     */
+    private String githubUserName;
+    /**
+     * Github的登录密码
+     */
+    private String githubPassword;
 
+    public String getPandocHome() {
+        return pandocHome;
+    }
+
+    public void setPandocHome(String pandocHome) {
+        this.pandocHome = pandocHome;
+    }
+
+    public String getWordBasePath() {
+        return wordBasePath;
+    }
+
+    public void setWordBasePath(String wordBasePath) {
+        this.wordBasePath = wordBasePath;
+    }
+
+    public String getBlogSendDate() {
+        return blogSendDate;
+    }
+
+    public void setBlogSendDate(String blogSendDate) {
+        this.blogSendDate = blogSendDate;
+    }
+
+    public String getGithubUserName() {
+        return githubUserName;
+    }
+
+    public void setGithubUserName(String githubUserName) {
+        this.githubUserName = githubUserName;
+    }
+
+    public String getGithubPassword() {
+        return githubPassword;
+    }
+
+    public void setGithubPassword(String githubPassword) {
+        this.githubPassword = githubPassword;
+    }
 }
