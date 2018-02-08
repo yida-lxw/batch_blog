f55d4bc3cb076fd99ccfb3b75c964741e6759975
yida <949226420@qq.com>
2018-02-08 21:40:46 +0800

    create httpclient config file

---
+++ src/main/java/com/yida/framework/blog/utils/httpclient/HttpClientConfig.java
@@ -0,0 +1,10 @@
+package com.yida.framework.blog.utils.httpclient;
+
+/**
+ * @Author Lanxiaowei
+ * @Date 2018-02-08 11:14
+ * @Description HttpClient相关配置
+ */
+public class HttpClientConfig {
+
+}
---
+++ src/main/java/com/yida/framework/blog/utils/httpclient/HttpClientConfigLoader.java
@@ -0,0 +1,23 @@
+package com.yida.framework.blog.utils.httpclient;
+
+/**
+ * @Author Lanxiaowei
+ * @Date 2018-02-08 11:15
+ * @Description HttpClient配置加载器
+ */
+public class HttpClientConfigLoader {
+
+
+    /**
+     * PoolingHttpClientConnectionManager poolingmgr = new PoolingHttpClientConnectionManager(
+     RegistryBuilder.<ConnectionSocketFactory>create()
+     .register("http", PlainConnectionSocketFactory.getSocketFactory())
+     .register("https", sslSocketFactoryCopy)
+     .build(),
+     null,
+     null,
+     null,
+     connTimeToLive,
+     connTimeToLiveTimeUnit != null ? connTimeToLiveTimeUnit : TimeUnit.MILLISECONDS);
+     */
+}
