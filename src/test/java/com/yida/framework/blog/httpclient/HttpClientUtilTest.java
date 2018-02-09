a606f6efda0571e90ecf1caeda6f7bb178fe6f87
yida <949226420@qq.com>
2018-02-09 23:23:37 +0800

    add Test case for The class of HttpClientUtil

--- src/test/java/com/yida/framework/blog/httpclient/HttpClientUtilTest.java
+++ src/test/java/com/yida/framework/blog/httpclient/HttpClientUtilTest.java
@@ -9,8 +9,10 @@ import com.yida.framework.blog.utils.httpclient.HttpClientUtil;
  */
 public class HttpClientUtilTest {
     public static void main(String[] args) {
-        String requestURL = "https://detail.tmall.com/item.htm?spm=a230r.1.14.27.210a82558PplkD&id=561751745746&ns=1&abbucket=14";
-        String content = HttpClientUtil.get(requestURL, "GBK");
-        System.out.println(content);
+        for (int i = 0; i < 10; i++) {
+            String requestURL = "https://detail.tmall.com/item.htm?spm=a230r.1.14.27.210a82558PplkD&id=561751745746&ns=1&abbucket=14";
+            String content = HttpClientUtil.get(requestURL, "GBK");
+            System.out.println(content);
+        }
     }
 }
