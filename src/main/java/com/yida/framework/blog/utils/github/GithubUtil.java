f1d51cac531f2ccf4755aced3fbb5fc0d2f973b4
yida <949226420@qq.com>
2018-02-10 22:39:49 +0800

    Complete the repository clone function via ssh and http,then the unit test pass.

--- src/main/java/com/yida/framework/blog/utils/github/GithubUtil.java
+++ src/main/java/com/yida/framework/blog/utils/github/GithubUtil.java
@@ -5,8 +5,8 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullCommand;
 import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
@@ -23,18 +23,30 @@ import java.io.IOException;
  * @Description Github操作工具类, 基于JGit封装
  */
 public class GithubUtil {
-    public static final String USER_HOME = System.getProperty("user.home");
     private static Logger log = LogManager.getLogger(GithubUtil.class.getName());
-    String localRepoPath;
-    String remoteRepoPath;
-    String localCodeDirectory;
+
+    public static final String USER_HOME = fixedPathDelimiter(System.getProperty("user.home"));
+    public static final String PRIVATE_KEY = USER_HOME + "/.ssh/id_rsa";
+
+    private static final ThreadLocal<SshSessionFactory> sshSessionFactoryThreadLocal = new ThreadLocal<>();
 
     public static void main(String[] args) throws IOException {
         String localRepositoryPath = "G:/git4test/blog";
-        String remoteRepoUrl = "git@github.com:yida-lxw/batch_blog.git";
+        String remoteRepoSSHUrl = "git@github.com:yida-lxw/blog.git";
+        String remoteRepoHttpUrl = "https://github.com/yida-lxw/blog.git";
+        String githubUserName = "yida-lxw";
+        String githubPassword = "你猜";
 
-        Git git = getGit(localRepositoryPath);
-        System.out.println("git:" + git);
+        //Git git = getGit(localRepositoryPath);
+
+
+        //git clone vi http url
+        //Git git = cloneRepositoryWithHttpAuth(remoteRepoHttpUrl, localRepositoryPath, githubUserName, githubPassword);
+        //System.out.println("git clone via http:" + git);
+
+        //git clone vi ssh url
+        Git git = cloneRepositoryWithSSHAuth(remoteRepoSSHUrl, localRepositoryPath);
+        System.out.println("git clone via ssh:" + git);
 
     }
 
@@ -115,33 +127,294 @@ public class GithubUtil {
         return git;
     }
 
-    public static void cloneRepositoryWithSSHAuth(Git git) {
+    /**
+     * 克隆远程仓库到本地仓库-Http协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param githubUserName      github登录账号
+     * @param githubPassword      github登录密码
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @param cloneAllBranches    是否克隆所有分支
+     * @param remote              设置远程仓库的映射名称,不设置的话,默认值为origin
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath,
+                                                  String githubUserName, String githubPassword,
+                                                  String branchName, boolean bare, boolean cloneAllBranches,
+                                                  String remote) {
+        Git git = null;
+        try {
+            CloneCommand cloneCommand = Git.cloneRepository()
+                    .setBare(bare)
+                    .setURI(remoteRepoPath)
+                    .setDirectory(new File(localRepositoryPath))
+                    .setCloneAllBranches(cloneAllBranches);
+            if (null != branchName && !"".equals(branchName)) {
+                cloneCommand = cloneCommand.setBranch("refs/heads/" + branchName);
+            }
+            if (null != githubUserName && !"".equals(githubUserName)) {
+                cloneCommand = cloneCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider(githubUserName, githubPassword));
+            }
+            if (null != remote && !"".equals(remote)) {
+                //The default value for "remote" is [origin]
+                cloneCommand = cloneCommand.setRemote(remote);
+            }
+            git = cloneCommand.call();
+        } catch (GitAPIException e) {
+            log.error("While Cloning the remote repository to local repository with remoteRepoPath-HTTP[{}] and localRepositoryPath[{}],we occur exception:\n{}",
+                    remoteRepoPath, localRepositoryPath, e.getMessage());
+        }
+        return git;
+    }
 
-        SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+    /**
+     * 克隆远程仓库到本地仓库
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址
+     * @param localRepositoryPath 本地仓库目录
+     * @param githubUserName      github登录账号
+     * @param githubPassword      github登录密码
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @param cloneAllBranches    是否克隆所有分支
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath,
+                                                  String githubUserName, String githubPassword,
+                                                  String branchName, boolean bare, boolean cloneAllBranches) {
+        return cloneRepositoryWithHttpAuth(remoteRepoPath, localRepositoryPath, githubUserName, githubPassword, branchName,
+                bare, cloneAllBranches, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址
+     * @param localRepositoryPath 本地仓库目录
+     * @param githubUserName      github登录账号
+     * @param githubPassword      github登录密码
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath,
+                                                  String githubUserName, String githubPassword,
+                                                  String branchName, boolean bare) {
+        return cloneRepositoryWithHttpAuth(remoteRepoPath, localRepositoryPath, githubUserName, githubPassword, branchName,
+                bare, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址
+     * @param localRepositoryPath 本地仓库目录
+     * @param githubUserName      github登录账号
+     * @param githubPassword      github登录密码
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath,
+                                                  String githubUserName, String githubPassword,
+                                                  String branchName) {
+        return cloneRepositoryWithHttpAuth(remoteRepoPath, localRepositoryPath, githubUserName, githubPassword, branchName,
+                false, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址
+     * @param localRepositoryPath 本地仓库目录
+     * @param githubUserName      github登录账号
+     * @param githubPassword      github登录密码
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath,
+                                                  String githubUserName, String githubPassword) {
+        return cloneRepositoryWithHttpAuth(remoteRepoPath, localRepositoryPath, githubUserName, githubPassword, null,
+                false, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址
+     * @param localRepositoryPath 本地仓库目录
+     * @return
+     */
+    public static Git cloneRepositoryWithHttpAuth(String remoteRepoPath, String localRepositoryPath) {
+        return cloneRepositoryWithHttpAuth(remoteRepoPath, localRepositoryPath, null, null, null, false, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param privateKey          本地私钥文件存放地址,默认值为C:/Users/Administrator/.ssh/id_rsa
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @param cloneAllBranches    是否克隆所有分支
+     * @param remote              设置远程仓库的映射名称,不设置的话,默认值为origin
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath,
+                                                 String privateKey, String branchName,
+                                                 boolean bare, boolean cloneAllBranches, String remote) {
+        SshSessionFactory sshSessionFactory = createSshSessionFactory(privateKey);
+        Git git = null;
+        try {
+            CloneCommand cloneCommand = Git.cloneRepository()
+                    .setBare(bare)
+                    .setURI(remoteRepoPath)
+                    .setDirectory(new File(localRepositoryPath))
+                    .setCloneAllBranches(cloneAllBranches);
+            if (null != branchName && !"".equals(branchName)) {
+                cloneCommand = cloneCommand.setBranch("refs/heads/" + branchName);
+            }
+            if (null != remote && !"".equals(remote)) {
+                //The default value for "remote" is [origin]
+                cloneCommand = cloneCommand.setRemote(remote);
+            }
+            if (null != sshSessionFactory) {
+                cloneCommand = cloneCommand.setTransportConfigCallback(new TransportConfigCallback() {
+                    @Override
+                    public void configure(Transport transport) {
+                        SshTransport sshTransport = (SshTransport) transport;
+                        sshTransport.setSshSessionFactory(sshSessionFactory);
+                    }
+                });
+            }
+            git = cloneCommand.call();
+        } catch (GitAPIException e) {
+            log.error("While Cloning the remote repository to local repository with remoteRepoPath-SSH[{}] and localRepositoryPath[{}],we occur exception:\n{}",
+                    remoteRepoPath, localRepositoryPath, e.getMessage());
+        }
+        return git;
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param privateKey          本地私钥文件存放地址,默认值为C:/Users/Administrator/.ssh/id_rsa
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @param cloneAllBranches    是否克隆所有分支
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath,
+                                                 String privateKey, String branchName,
+                                                 boolean bare, boolean cloneAllBranches) {
+        return cloneRepositoryWithSSHAuth(remoteRepoPath, localRepositoryPath, privateKey, branchName, bare, cloneAllBranches, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param privateKey          本地私钥文件存放地址,默认值为C:/Users/Administrator/.ssh/id_rsa
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @param bare                是否只初始化远程仓库至本地,但不下载远程仓库里的任何文件至本地仓库
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath,
+                                                 String privateKey, String branchName, boolean bare) {
+        return cloneRepositoryWithSSHAuth(remoteRepoPath, localRepositoryPath, privateKey, branchName, bare, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param privateKey          本地私钥文件存放地址,默认值为C:/Users/Administrator/.ssh/id_rsa
+     * @param branchName          分支名称即你需要克隆哪个分支,默认为master分支
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath,
+                                                 String privateKey, String branchName) {
+        return cloneRepositoryWithSSHAuth(remoteRepoPath, localRepositoryPath, privateKey, branchName, false, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @param privateKey          本地私钥文件存放地址,默认值为C:/Users/Administrator/.ssh/id_rsa
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath, String privateKey) {
+        return cloneRepositoryWithSSHAuth(remoteRepoPath, localRepositoryPath, privateKey, null, false, false, null);
+    }
+
+    /**
+     * 克隆远程仓库到本地仓库-SSH协议方式
+     *
+     * @param remoteRepoPath      远程Github仓库的URL地址-Http格式
+     * @param localRepositoryPath 本地仓库目录
+     * @return
+     */
+    public static Git cloneRepositoryWithSSHAuth(String remoteRepoPath, String localRepositoryPath) {
+        return cloneRepositoryWithSSHAuth(remoteRepoPath, localRepositoryPath, null, null, false, false, null);
+    }
+
+    /**
+     * 关闭Git实例,释放文件句柄资源
+     *
+     * @param git
+     */
+    public static void closeGit(Git git) {
+        if (null != git) {
+            git.close();
+        }
+    }
+
+    /**
+     * 创建SshSessionFactory实例对象
+     *
+     * @param privateKey 本地私钥文件的存放地址
+     * @return
+     */
+    private static SshSessionFactory createSshSessionFactory(String privateKey) {
+        SshSessionFactory sshSessionFactory = sshSessionFactoryThreadLocal.get();
+        if (null != sshSessionFactory) {
+            return sshSessionFactory;
+        }
+        if (null == privateKey || "".equals(privateKey)) {
+            privateKey = PRIVATE_KEY;
+        }
+        final String finalPrivateKey = privateKey;
+        sshSessionFactory = new JschConfigSessionFactory() {
             @Override
             protected void configure(OpenSshConfig.Host host, Session session) {
                 session.setConfig("StrictHostKeyChecking", "no");
             }
 
             @Override
-            protected JSch getJSch(final OpenSshConfig.Host hc, FS fs) throws JSchException {
-                JSch jsch = super.getJSch(hc, fs);
+            protected JSch getJSch(final OpenSshConfig.Host host, FS fs) throws JSchException {
+                JSch jsch = super.getJSch(host, fs);
                 jsch.removeAllIdentity();
-                jsch.addIdentity("/path/to/private/key");
+                jsch.addIdentity(finalPrivateKey);
                 return jsch;
             }
         };
+        return sshSessionFactory;
+    }
 
-        String privateKey = USER_HOME + "/.ssh/id_rsa";
-        PullCommand pull = git.pull().setTransportConfigCallback(new TransportConfigCallback() {
-
-            @Override
-
-            public void configure(Transport transport) {
-                SshTransport sshTransport = (SshTransport) transport;
-                sshTransport.setSshSessionFactory(sshSessionFactory);
-            }
-        });
+    /**
+     * 创建SshSessionFactory实例对象
+     *
+     * @return
+     */
+    private static SshSessionFactory createSshSessionFactory() {
+        return createSshSessionFactory(null);
     }
 
     /**
